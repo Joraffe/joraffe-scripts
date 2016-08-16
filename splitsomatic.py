7da92cd85bf9cd935cb0acecda923d329b992df9
Joraffe <jordan@humblebundle.com>
2016-08-16 16:58:14 -0700

    v2 of splitsomatic.py
    - Replaced sibling_splits_add_to_one() with polish_subsplits() for simplicity.
    - Combined add_partner_splits() and extract_payee_info() into a single function add_subsplits()
    -- This makes the iteration much simpler (no longer need enumerate)
    - Added function add_partner_splits()
    - General cleanup in execution part of the script (if '__name__' == __main__) 
    - Added trailing commas where needed

--- splitsomatic.py
+++ splitsomatic.py
@@ -3,7 +3,6 @@ import argparse
 import csv
 
 from decimal import getcontext
-from decimal import localcontext
 from decimal import Decimal
 from libraries.aetycoon.prettydata import prettify
 
@@ -14,7 +13,7 @@ parser.add_argument(
   help='The csv file containing information about the splits from the SOTB')
 args = parser.parse_args()
 
-base_splits = {
+template_splits = {
   'initial': {
     'order': [
       {
@@ -28,7 +27,12 @@ base_splits = {
         'name': 'Charity',
         'sibling_split': Decimal('0.15'),
         'subsplit': [],
-      }
+      },
+      {
+        'class': 'humblebundle',
+        'name': 'Humble Tip',
+        'sibling_split': Decimal('0.20'),
+      },
     ]
   },
   'mpa': {
@@ -44,22 +48,21 @@ base_splits = {
         'name': 'Charity',
         'sibling_split': Decimal('0.15'),
         'subsplit': [],
-      }
+      },
+      {
+        'class': 'humblebundle',
+        'name': 'Humble Tip',
+        'sibling_split': Decimal('0.20'),
+      },
     ]
-  }
+  },
 }
 
-cyoc_splits = {
+cyoc_subsplit = {
   'class': 'paypalgivingfund',
   'name': 'Chose your own charity',
   'secondary_id': 'cyoc',
   'sibling_split': Decimal('0.0'),
-}
-
-base_humbletip = {
-  'class': 'humblebundle',
-  'name': 'Humble Tip',
-  'sibling_split': Decimal('0.20'),
 }
 
 partner_splits = {
@@ -69,143 +72,124 @@ partner_splits = {
   'partner_split': Decimal('0.15'),
 }
 
-partner_ajusted_splits = [
-  # 'partner_split' for Devs/Pubs:
-  Decimal('0.60'),
-  # 'partner_split' for Charity:
-  Decimal('0.10'),
-  # 'partner_split' for Humble:
-  Decimal('0.15'),
-]
-
 
 def add_partner_splits(splits):
-  # TO DO -- Needs some work
-  pass
+  splits['initial']['order'].append(partner_splits)
+  splits['initial']['order'][0]['partner_split'] = Decimal('0.60')
+  splits['initial']['order'][1]['partner_split'] = Decimal('0.10')
+  splits['initial']['order'][2]['partner_split'] = Decimal('0.15')
+  if 'mpa' in splits:
+    splits['mpa']['order'].append(partner_splits)
+    splits['mpa']['order'][0]['partner_split'] = Decimal('0.60')
+    splits['mpa']['order'][1]['partner_split'] = Decimal('0.10')
+    splits['mpa']['order'][2]['partner_split'] = Decimal('0.15')
 
 
-def add_split_information(payee_info, base_splits, identifier):
-  getcontext().prec = 8
-  base_order_index = base_splits[identifier]['order']
-  for payee_type_index in range(len(base_order_index)):
-    subsplits = base_order_index[payee_type_index]['subsplit']
-    csv_payee_info = payee_info[payee_type_index]
-    num_payees = len(payee_info[payee_type_index])
-    for payee_info_index in range(num_payees):
-      if len(payee_info) == 1:
-        del subsplits
-        break
-      else:
-        subsplits.append(
-          {
-            'class': csv_payee_info[payee_info_index]['payee'],
-            'name': csv_payee_info[payee_info_index]['human-name'],
-            'secondary_id': csv_payee_info[payee_info_index]['secondary_id']
-          }
-        )
-    raw_sibling_splits = []
-    for sibling_split in range(num_payees):
-      raw_sibling_splits.append(Decimal('1.0') / Decimal(num_payees))
-    sibling_index = 0
-    adjusted_sibling_splits = sibling_splits_add_to_one(raw_sibling_splits)
-    for subsplit in subsplits:
-      subsplit['sibling_split'] = adjusted_sibling_splits[sibling_index]
-      if subsplit['secondary_id'] == '0':
+def polish_subsplits(subsplits):
+  '''
+  Polishes the subsplits for Devs/Charity to remove extraneous 'secondary_id'
+  and to have the sum of the Decimal value equal to exactly 1.
+  For example:
+  Decimal(0.333333333)                      Decimal(0.333333334)
+  Decimal(0.333333333)          -->         Decimal(0.333333333)
+  Decimal(0.333333333)                      Decimal(0.333333333)
+  '''
+  getcontext().prec = 10
+  sum_sibling_splits = Decimal('0.0')
+  for subsplit in subsplits:
+    if subsplit['secondary_id'] is '0':
         del subsplit['secondary_id']
-      sibling_index += 1
-  return base_splits[identifier]
+    sum_sibling_splits += subsplit['sibling_split']
+  diff = Decimal('1.0') - sum_sibling_splits
+  subsplits[0]['sibling_split'] += diff
+  return subsplits
 
 
-def sibling_splits_add_to_one(sibling_splits):
-  list_of_sibling_splits = sibling_splits
-  sum_of_sibling_splits = Decimal('0.0')
-  for sibling_split in list_of_sibling_splits:
-    with localcontext() as ctx2:
-      ctx2.prec = 10
-      sum_of_sibling_splits += sibling_split
-  diff = Decimal('1.0') - sum_of_sibling_splits
-  if diff == Decimal('0.0'):
-    return list_of_sibling_splits
+def add_subsplits(csvinfo, splits, identifier):
+  '''
+  Extracts the payment information for a Bundle from the .CSV file
+  that has been exported from a given SOTB and generates the subsplits
+  for both Devs/Pubs and Charities involved.
+
+  Takes a .CSV file (csvinfo) and a splits template (empty subsplit list)
+  and returns splits with an unpolished/raw subsplit list.
+  '''
+  dev_subsplits = []
+  charity_subsplits = []
+  for item in csvinfo:
+    if item['partner-name'] is not '':
+      dev_subsplits.append(
+        {
+          'class': item['partner-payee'],
+          'name': item['partner-name'],
+          'secondary_id': item['partner-secondary-id'],
+        }
+      )
+    if item['charity-name'] is not '' and item['charity-secondary-id'] is not 'cyoc':
+      charity_subsplits.append(
+        {
+          'class': item['charity-name'],
+          'name': item['charity-payee'],
+          'secondary_id': item['charity-secondary-id'],
+        }
+      )
+  getcontext().prec = 9
+  for dev in dev_subsplits:
+    dev['sibling_split'] = Decimal('1.0') / Decimal(len(dev_subsplits))
+  for charity in charity_subsplits:
+    charity['sibling_split'] = Decimal('1.0') / Decimal(len(charity_subsplits))
+
+  # Polish subsplits: remove extraneous 'secondary_id' & sibling_splits add to 1
+  dev_subsplits = polish_subsplits(dev_subsplits)
+  charity_subsplits = polish_subsplits(charity_subsplits)
+
+  # Add CYOC to charities subsplits if needed
+  if csvinfo[0]['cyoc-bool'] is '1':
+    charity_subsplits.append(cyoc_subsplit)
+
+  devs = splits[identifier]['order'][0]
+  charities = splits[identifier]['order'][1]
+
+  # If there is only 1 dev/pub or charity, subsplits no longer needed
+  if len(dev_subsplits) is 1:
+    del devs['subsplit']
+    devs['class'] = dev_subsplits[0]['class']
+    devs['name'] = dev_subsplits[0]['name']
   else:
-    diff_count = abs(int(diff / Decimal('1.0E-9')))
-    diff_adjust = abs(diff / Decimal(diff_count))
-    sibling_split_index = 0
-    for sibling_split in range(diff_count):
-      if diff < 0.0:
-        list_of_sibling_splits[sibling_split_index] -= diff_adjust
-      elif diff > 0.0:
-        list_of_sibling_splits[sibling_split_index] += diff_adjust
-      sibling_split_index += 1
-  return list_of_sibling_splits
-
-
-def extract_payee_info(csvinfo):
-  payee_info = [
-    # partner_info
-    [],
-    # charity_info
-    [],
-  ]
-  for payee in csvinfo:
-    if payee['partner-name'] == '':
-      continue
-    else:
-      payee_info[0].append(
-        {
-          'human-name': payee['partner-name'],
-          'payee': payee['partner-payee'],
-          'secondary_id': payee['partner-secondary-id'],
-          'mpa-bool': payee['mpa-bool'],
-        }
-      )
-    if payee['charity-name'] == '':
-      continue
-    else:
-      payee_info[1].append(
-        {
-          'human-name': payee['charity-name'],
-          'payee': payee['charity-payee'],
-          'secondary_id': payee['charity-secondary-id'],
-        }
-      )
-  return payee_info
+    devs['subsplit'] = dev_subsplits
+  if len(charity_subsplits) is 1:
+    del charities['subsplit']
+    charities['class'] = charity_subsplits[0]['class']
+    charities['name'] = charity_subsplits[0]['name']
+  else:
+    charities['subsplit'] = charity_subsplits
+  return splits
 
 
 if __name__ == '__main__':
   initial_csvinfo = []
   mpa_csvinfo = []
-  splits = base_splits.copy()
+  splits = template_splits.copy()
 
   with open(args.csvfile) as csvfile:
     reader = csv.DictReader(csvfile)
     for row in reader:
-      if row['mpa-bool'] == '0':
+      if row['mpa-bool'] is '0':
         initial_csvinfo.append(row)
         mpa_csvinfo.append(row)
-      elif row['mpa-bool'] == '1':
+      elif row['mpa-bool'] is '1':
         mpa_csvinfo.append(row)
 
   # Extract and add initial splits
-  initial_payee_info = extract_payee_info(initial_csvinfo)
-  splits['initial'] = add_split_information(initial_payee_info, splits, 'initial')
-  splits['initial']['order'].append(base_humbletip)
-
-  # Extract and add mpa splits
-  mpa_payee_info = extract_payee_info(mpa_csvinfo)
-  splits['mpa'] = add_split_information(mpa_payee_info, splits, 'mpa')
-  splits['mpa']['order'].append(base_humbletip)
-
-  # Add CYOC if needed
-  if initial_csvinfo[0]['cyoc-bool'] == '1':
-    splits['initial']['order'][1]['subsplit'].append(cyoc_splits)
-    splits['mpa']['order'][1]['subsplit'].append(cyoc_splits)
+  splits = add_subsplits(initial_csvinfo, splits, 'initial')
+  # Extract and add mpa splits, if needed
+  if len(initial_csvinfo) is not len(mpa_csvinfo):
+    splits = add_subsplits(mpa_csvinfo, splits, 'mpa')
+  else:
+      del splits['mpa']
 
   # Add Bundle Partner Splits if needed (TO DO)
-  # if initial_csvinfo[0]['bundle-partners-bool'] == '1':
-  #  add_partner_splits(splits)
-
-  # Delete mpa splits if not needed
-  if splits['initial'] == splits['mpa']:
-    del splits['mpa']
+  if initial_csvinfo[0]['bundle-partners-bool'] is '1':
+    add_partner_splits(splits)
 
   print ''.join(prettify(splits))
